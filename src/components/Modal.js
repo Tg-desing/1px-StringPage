e02336ea914690265f0494a7e398a349a91e915e
Taegon <taegon@cusee.net>
2023-09-19 20:07:00 +0900

    update modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,37 +2,38 @@ import classes from './Modal.module.css';
 import ReactDOM from 'react-dom';
 
 const Backdrop = () => {
-  return <div className={classes.backdrop}></div>;
+	const b = 0;
+	return <div className={classes.backdrop}></div>;
 };
 
 const OverlayModal = () => {
-  return (
-    <div className={classes['calender-pos']}>
-      <img src={calender} className={classes.calender}></img>
-      <div className={classes['calender-container']}>
-        {setImgList(imageUrlList)}
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes['calender-pos']}>
+			<img src={calender} className={classes.calender}></img>
+			<div className={classes['calender-container']}>
+				{setImgList(imageUrlList)}
+			</div>
+		</div>
+	);
 };
 
 const Modal = (props) => {
-  const onClickBackdropHandler = () => {
-    props.onClickBackdrop();
-  };
+	const onClickBackdropHandler = () => {
+		props.onClickBackdrop();
+	};
 
-  return (
-    <>
-      {ReactDOM.createPortal(
-        <Backdrop onClick={onClickBackdropHandler}></Backdrop>,
-        document.getElementById('backdrop-root')
-      )}
-      {ReactDOM.createPortal(
-        <OverlayModal></OverlayModal>,
-        document.getElementById('overlay-root')
-      )}
-    </>
-  );
+	return (
+		<>
+			{ReactDOM.createPortal(
+				<Backdrop onClick={onClickBackdropHandler}></Backdrop>,
+				document.getElementById('backdrop-root')
+			)}
+			{ReactDOM.createPortal(
+				<OverlayModal></OverlayModal>,
+				document.getElementById('overlay-root')
+			)}
+		</>
+	);
 };
 
 export default Modal;
